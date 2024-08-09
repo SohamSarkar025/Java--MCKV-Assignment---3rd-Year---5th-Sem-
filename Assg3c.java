a6540bb5d2ed6c909f683909dbca18f32ed0707f
Soham <srutidhara153@gmail.com>
2024-08-09 17:40:01 +0530

    update

--- Assg3c.java
+++ Assg3c.java
@@ -8,17 +8,34 @@
 //Else Normal year.
 
 import java.util.Scanner;
-public class Assg3c {
+class Assg3c{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the year: ");
+        System.out.println("Enter the year:");
         int year = in.nextInt();
+        boolean leap=false;
         in.close();
-        if( year % 4 ==0 && year % 400 ==0 || year%100!=0 ){
-            System.out.println("This is a leap year.");
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    leap=true;
+                }
+                else{
+                    leap=false;
+                }
+            }
+            else{
+                leap=true;
+            }
         }
         else{
-            System.out.println("This is not a leap year.");
+            leap=false;
+        }
+        if(leap){
+            System.out.println(year+" is a leap year.");
+        }
+        else{
+            System.out.println(year+" is not a leap year.");
         }
     }
 }
--- Practicedemo.java
+++ Practicedemo.java
@@ -1,52 +1,33 @@
-
-
-
-//Write a java program to generate Fibonacci Series up-to n terms using loop. 
-
-// import java.util.Scanner;
-// class Practicedemo{
-//          public static void main(String[] args){
-//              Scanner in = new Scanner(System.in);
-//              System.out.println("Enter the number: ");
-//              int num = in.nextInt();
-//              in.close();
-//              System.out.println("Fibonacci Series up-to "+num+"terms using loop.");
-//              int num1=0,num2=1;
-//              for(int i=1;i<=num;i++){
-//                 System.out.print(num1+", ");
-//                 int num3=num1+num2;
-//                 num1=num2;
-//                 num2=num3;
-//             }
-//         }
-// }
-
-
-//Write a java program to reverse a number and check whether it is a Palindrome. 
-
-// import java.util.Scanner;
-// class Practicedemo{
-//          public static void main(String[] args){
-//             int sum=0,r,temp;
-//              Scanner in = new Scanner(System.in);
-//              System.out.println("Enter the number");
-//              int num=in.nextInt();
-//              in.close();
-//              temp=num;
-//              while(num>0){
-//                 r=(num%10);
-//                 sum=(sum*10)+r;
-//                 num=num/10;
-//             }
-//              if(temp==sum){
-//                 System.out.println("The reverse of the number is "+sum+" This is palindrome");
-//              }
-//              else{
-//                 System.out.println("The reverse of the number is "+sum+" This is not palindrome");
-
-//             }
-//         }
-// }
-
-
+import java.util.Scanner;
+class Practicedemo{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the year:");
+        int year = in.nextInt();
+        boolean leap=false;
+        in.close();
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    leap=true;
+                }
+                else{
+                    leap=false;
+                }
+            }
+            else{
+                leap=true;
+            }
+        }
+        else{
+            leap=false;
+        }
+        if(leap){
+            System.out.println(year+" is a leap year.");
+        }
+        else{
+            System.out.println(year+" is not a leap year.");
+        }
+    }
+}
 
